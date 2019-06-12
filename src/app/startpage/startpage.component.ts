9cb9fa15c9a312618bfbda63b64d8c6f0e54478e
i316873 <i316873@fhict.nl>
2019-06-12 19:18:40 +0200

    what is browserlist, also updated angular to 8 and added websocket rxjs

--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -5,7 +5,8 @@ import {Router} from '@angular/router';
 import {StartpageService} from '../services/startpage.service';
 import {User} from '../../Models/User';
 import {Kweet} from '../../Models/Kweet';
-import {__await} from 'tslib';
+import {webSocket} from 'rxjs/webSocket';
+import {create} from 'domain';
 
 @Component({
   selector: 'app-startpage',
@@ -23,19 +24,29 @@ export class StartpageComponent implements OnInit {
   kweets: Kweet[];
   searchedKweets: object;
   createdKweet: Kweet;
+  subject;
 
   constructor(
     private formbuilder: FormBuilder,
     private startpageService: StartpageService,
     private router: Router,
     private profilepageService: ProfileService,
-    private ref: ChangeDetectorRef) { }
+    private ref: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
     this.startPage = this.formbuilder.group({
       searchedKweet: ['', Validators.required],
       createdKweet: ['', Validators.required]
     });
+
+    this.subject = webSocket('ws://localhost:8080/WebLogEJB_Finished-1.0-SNAPSHOT/echo-socket');
+
+    this.subject.subscribe(
+      msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
 
     this.user = JSON.parse(localStorage.getItem('loggedUser'));
 
@@ -44,7 +55,9 @@ export class StartpageComponent implements OnInit {
     this.refresh();
   }
 
-  get f() {return this.startPage.controls; }
+  get f() {
+    return this.startPage.controls;
+  }
 
   search() {
     this.startpageService.search(this.f.searchedKweet.value).subscribe(data => {
@@ -53,10 +66,13 @@ export class StartpageComponent implements OnInit {
   }
 
   createKweet() {
-      this.startpageService.createTweet(this.f.createdKweet.value, this.user.userId).subscribe(createdkweet => {
-        this.kweets.push(createdkweet);
-      });
-      this.ref.detectChanges();
+    this.f.createdKweet;
+    this.startpageService.createTweet(this.f.createdKweet.value, this.user.userId).subscribe(createdkweet => {
+      this.kweets.push(createdkweet);
+      this.subject.next(createdkweet);
+    });
+
+    this.ref.detectChanges();
   }
 
   refresh() {
@@ -73,10 +89,10 @@ export class StartpageComponent implements OnInit {
     localStorage.setItem('visitedUser', JSON.stringify(this.visitedUser));
     this.router.navigate(['profilepage']);
   }
+
   logout() {
 
   }
-
 
 
 }
